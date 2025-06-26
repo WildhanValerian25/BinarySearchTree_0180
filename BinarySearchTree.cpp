e0befbf21b809b3afc35ee39761d903d631ee16d
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-06-26 11:14:41 +0700

    Menambahkan fungsi isEmpty()

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -134,4 +134,10 @@ void search(int element, node *&parent, node *&currentnode)
         cout << ptr->info << " ";
     }    
 
-   
+    bool isEmpty()
+    {
+        // Checks if the tree is empty
+        return ROOT == nullptr;
+    }
+};
+
